98aaa2b891a6bc03ba857ac91fbbf2dd1ce01ed0
KurobaneYan <yan.kurobane@gmail.com>
2017-04-19 13:45:43 +0300

    Init commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# ZTCars
+
+## dependences
+
+git, npm, angular, node, express, mongodb, mongoose, mongoose-auto-increment
+
+## installation 
+
+npm install
---
+++ server.js
@@ -0,0 +1,25 @@
+let port = 1337;
+let express = require('express');
+let mongoose = require('mongoose');
+let autoIncrement = require('mongoose-auto-increment');
+let fs = require('fs');
+let path = require('path');
+let app = express();
+
+let databaseUrl = 'mongodb://localhost/ZTCars';
+
+mongoose.connect(databaseUrl);
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function () {
+    console.log('Mongoose connected to ' + databaseUrl);
+});
+
+app.use('/', function (req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+app.listen(port, function () {
+    console.log('Server running at http://localhost:' + port);
+});
