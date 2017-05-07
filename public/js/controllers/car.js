8ee2039e34cee4500be35064b60e311651856717
Kurobane Yan <yan.kurobane.@gmail.com>
2017-05-07 11:58:20 +0300

    add controller, which i forget to include in previous commit

---
+++ public/js/controllers/car.js
@@ -0,0 +1,8 @@
+module.exports = function($scope, $routeParams, userService) {
+    $scope.car = null;
+    userService.getCarById($routeParams.carId).then(function(resp) {
+        $scope.car = resp.data;
+    }, function(error) {
+        console.log(error);
+    });
+};
